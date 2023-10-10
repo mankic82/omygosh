4fd9605968bed79baf091f53388c84b2a8aa7845
mankic <gacci@naver.com>
2023-10-10 23:33:38 +0900

    ..

--- p4_scheduler.py
+++ p4_scheduler.py
@@ -6,10 +6,18 @@ import psycopg2
 from psycopg2 import OperationalError
 from time import sleep
 import os
+import time
+import logging
+
+# 로깅 설정
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 timeout = int(os.environ.get('REQUEST_TIMEOUT', 30))
 
+# 글로벌 변수 설정
 HOST = 'http://172.30.1.84:8081'
+MAX_RETRIES = 3  
+MAX_DB_RETRIES = 3
 
 def uploadfile_to_vm(vm_name: str, local_path: str, remote_path: str, retries=5):
     print("파일을 VM에 업로드 중...")
@@ -17,7 +25,7 @@ def uploadfile_to_vm(vm_name: str, local_path: str, remote_path: str, retries=5)
         try:
             with open(local_path, 'rb') as f:
                 files = {'file': f}
-                response = requests.post(f'{HOST}/upload/{remote_path}', files=files, timeout=60, verify=False)
+                response = requests.post(f'{HOST}/upload/{remote_path}', files=files, timeout=180, verify=False)
             print("파일 업로드 완료.")
             return
         except requests.exceptions.ConnectionError:
@@ -28,18 +36,24 @@ def uploadfile_to_vm(vm_name: str, local_path: str, remote_path: str, retries=5)
             print(f"An unexpected error occurred: {e}")
             return
 
-def exec_remote_path(vm_name: str, remote_path: str, argument: str, timeout: int):
+def exec_remote_path(vm_name: str, remote_path: str, argument: str, timeout: int, retries=5):
     print("원격 경로 실행 중...")
-    try:
-        data = {"command": remote_path, "arg": argument}
-        headers = {'Content-type': 'application/json'}
-        response = requests.post(f'{HOST}/command', data=json.dumps(data), headers=headers, timeout=timeout)
-        if response.status_code == 200:
-            print("원격 경로 실행 완료.")
-        else:
-            print(f"Failed to execute remote path. Status code: {response.status_code}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    while retries > 0:
+        try:
+            data = {"command": remote_path, "arg": argument}
+            headers = {'Content-type': 'application/json'}
+            response = requests.post(f'{HOST}/command', data=json.dumps(data), headers=headers, timeout=timeout)
+            if response.status_code == 200:
+                print("원격 경로 실행 완료.")
+                return
+            else:
+                print(f"Failed to execute remote path. Status code: {response.status_code}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        
+        print("Retrying...")
+        retries -= 1
+        sleep(1)
 
 # 이벤트 로그 처리
 def exec_event_export(vm_name: str):
@@ -58,12 +72,18 @@ def download_remote_file(vm_name: str, remote_path: str, local_path: str):
 
 # 데이터베이스 연결
 def connect_db(db_address: str):
-    try:
-        conn = psycopg2.connect(db_address)
-        return conn, None
-    except OperationalError as e:
-        return None, str(e)
+    retries = 0
+    while retries < MAX_DB_RETRIES:
+        try:
+            conn = psycopg2.connect(db_address, client_encoding='utf8')
+            return conn, None
+        except OperationalError as e:
+            print(f"데이터베이스 연결 실패. 재시도 중... ({retries + 1})")
+            retries += 1
+            time.sleep(1)
+    return None, "Max retries reached for DB connection"
 
+# 데이터베이스 연결 종료
 def close_db(db_handle):
     try:
         db_handle.close()
@@ -71,6 +91,7 @@ def close_db(db_handle):
     except Exception as e:
         print(f"An error occurred while closing the database: {e}")
 
+# 분석 시작
 def start_analyze(vm_name: str, file_path:str, argument:str, timeout:int):
     # 가상 머신 시작
     start_vm(vm_name)  
@@ -92,7 +113,7 @@ def start_analyze(vm_name: str, file_path:str, argument:str, timeout:int):
     rollback_vm(vm_name, "<testsnap>")
     
 def get_db_handler():
-    db_address = "postgresql://postgres:0984@localhost:5432/event_logs"
+    db_address = os.environ.get('DB_ADDRESS', "postgresql://postgres:0984@localhost:5432/postgres")
     db_handler, error = connect_db(db_address)
     if db_handler:
         return db_handler, None
@@ -108,7 +129,7 @@ if __name__ == '__main__':
         exit(1)
 
     analyze_target_path = r'C:\Users\USER\Documents\Koino\Log\KASSvcMgr_AnySupportService.log'
-    vm_name = 'Win-11'
+    vm_name = 'Win11'
     argument = ''
     timeout = 180
 
--- p4_vmbox.py
+++ p4_vmbox.py
@@ -2,15 +2,18 @@ import subprocess
 
 def start_vm(vm_name: str) -> bool:
     try:
-        subprocess.run(["VBoxManage", "startvm", vm_name])
+        result = subprocess.run(["VBoxManage", "startvm", vm_name], capture_output=True)
         return True
+    except FileNotFoundError:
+        print("VBoxManage 명령어를 찾을 수 없습니다. VirtualBox가 설치되어 있는지 확인해주세요.")
+        return False
     except Exception as e:
-        print(f"Error starting VM: {e}")
+        print(f"가상 머신을 시작하는 중에 오류가 발생했습니다: {e}")
         return False
 
 def stop_vm(vm_name: str) -> bool:
     try:
-        subprocess.run(["VBoxManage", "controlvm", vm_name, "poweroff"])
+        result = subprocess.run(["VBoxManage", "controlvm", vm_name, "poweroff"])
         return True
     except Exception as e:
         print(f"Error stopping VM: {e}")
@@ -27,7 +30,7 @@ def list_vm() -> list:
 
 def snapshot_vm(vm_name: str, snapshot_name: str) -> bool:
     try:
-        subprocess.run(["VBoxManage", "snapshot", vm_name, "take", snapshot_name])
+        result = subprocess.run(["VBoxManage", "snapshot", vm_name, "take", snapshot_name])
         return True
     except Exception as e:
         print(f"Error taking snapshot: {e}")
@@ -35,29 +38,33 @@ def snapshot_vm(vm_name: str, snapshot_name: str) -> bool:
 
 def rollback_vm(vm_name: str, snapshot_name: str) -> bool:
     try:
-        subprocess.run(["VBoxManage", "snapshot", vm_name, "restore", snapshot_name])
+        result = subprocess.run(["VBoxManage", "snapshot", vm_name, "restore", snapshot_name])
         return True
     except Exception as e:
         print(f"Error restoring snapshot: {e}")
         return False
 
-# 메인 실행 부분 시작
-if True:  # virtualbox_dir 변수를 사용하지 않으므로, 단순히 if True로 변경합니다.
+if __name__ == "__main__":
     print(f"C:\\Program Files\\Oracle\\VirtualBox")
-    
-    vm_list = list_vm()  # 원래의 list_vm 함수를 직접 호출합니다.
+    vm_list = list_vm() 
     print("CLI로 조회한 가상 머신 목록:")
-    
     for i, vm in enumerate(vm_list, start=1):
         print(f"{i}. {vm}")
 
-    selected_vm_index = int(input("제어할 가상 머신 번호를 입력하세요: ")) - 1
+    while True:
+        try:
+            selected_vm_index = int(input("제어할 가상 머신 번호를 입력하세요: ")) - 1
+            if 0 <= selected_vm_index < len(vm_list):
+                selected_vm_name = vm_list[selected_vm_index]  # 이름만 추출
+            else:
+                print("유효하지 않은 번호입니다.")
+                continue
+        except ValueError:
+            print("잘못된 번호를 입력했습니다. 숫자를 입력해주세요.")
+            continue
 
-    if 0 <= selected_vm_index < len(vm_list):
-        selected_vm_name = vm_list[selected_vm_index]  # 이름만 추출
-        
-        action = input("1: 가상 머신 시작\n2: 가상 머신 종료\n3: 스냅샷 생성\n4: 스냅샷 복구\n수행할 작업을 선택하세요: ")
-        
+        action = input("1: 가상 머신 시작\\n2: 가상 머신 종료\\n3: 스냅샷 생성\\n4: 스냅샷 복구\\n수행할 작업을 선택하세요: ")
+
         if action == '1':
             print(start_vm(selected_vm_name))
         elif action == '2':
@@ -70,5 +77,7 @@ if True:  # virtualbox_dir 변수를 사용하지 않으므로, 단순히 if Tru
             print(rollback_vm(selected_vm_name, snapshot_name))
         else:
             print("잘못된 작업을 선택했습니다.")
-else:
-    print("VirtualBox 설치 디렉터리를 찾을 수 없습니다.")
+
+        continue_choice = input("계속하시겠습니까? (y/n): ")
+        if continue_choice.lower() != 'y':
+            break
